73021a082a50b33cc58388da2673dbb665f1a949
Haneen Aljamal <hannoneh90@hotmail.com>
2026-02-26 16:50:48 +0300

    Update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p "$reads"
+
 # reference files
 ref="/home/hannoneh90/variant-calling/supporting_files/hg38/hg38.fa"
 known_sites="/home/hannoneh90/variant-calling/supporting_files/hg38/GCF_000001405.40.gz"
