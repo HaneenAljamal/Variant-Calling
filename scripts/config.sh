e1f82fe5665cfa3b8c10c14762b22e4f2bb51090
HaneenAljamal <hannoneh90@hotmail.com>
2026-02-26 16:44:08 +0300

    add config.sh

---
+++ scripts/config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# reference files
+ref="/home/hannoneh90/variant-calling/supporting_files/hg38/hg38.fa"
+known_sites="/home/hannoneh90/variant-calling/supporting_files/hg38/GCF_000001405.40.gz"
+
+# directories
+reads="/home/hannoneh90/variant-calling/reads"
+aligned_reads="/home/hannoneh90/variant-calling/aligned_reads"
+results="/home/hannoneh90/variant-calling/results"
