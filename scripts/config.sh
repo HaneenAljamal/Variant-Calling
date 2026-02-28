1ec0facfcb3be4e056cef742b10ac3b692112847
Haneen Aljamal <hannoneh90@hotmail.com>
2026-02-28 20:29:28 +0300

    Update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -2,7 +2,7 @@
 
 # reference files
 ref="/home/hannoneh90/variant-calling/supporting_files/hg38/hg38.fa"
-known_sites="/home/hannoneh90/variant-calling/supporting_files/hg38/Homo_sapiens_assembly38.dbsnp138.vcf.gz"
+known_sites="/home/hannoneh90/variant-calling/supporting_files/hg38/GCF_000001405.40.gz"
 
 # directories
 reads="/home/hannoneh90/variant-calling/reads"
