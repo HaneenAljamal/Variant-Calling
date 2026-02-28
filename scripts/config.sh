a219f97263037622d13139fa0df25b7021d236c6
Haneen Aljamal <hannoneh90@hotmail.com>
2026-02-28 17:32:43 +0300

    Update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -8,7 +8,8 @@ known_sites="/home/hannoneh90/variant-calling/supporting_files/hg38/GCF_00000140
 reads="/home/hannoneh90/variant-calling/reads"
 aligned_reads="/home/hannoneh90/variant-calling/aligned_reads"
 results="/home/hannoneh90/variant-calling/results"
+data="/home/hannoneh90/variant-calling/data"
 ref_dir="/home/hannoneh90/variant-calling/supporting_files/hg38"
 
 # create directories
-mkdir -p "$reads" "$aligned_reads" "$results" "$ref_dir"
+mkdir -p "$reads" "$aligned_reads" "$results" "$ref_dir" "$data"
