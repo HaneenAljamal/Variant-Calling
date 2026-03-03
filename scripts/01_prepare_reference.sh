3820db4289350f3d5e4680c8f146b0332c388e1c
Haneen Aljamal <hannoneh90@hotmail.com>
2026-03-03 21:29:16 +0300

    Update 01_prepare_reference.sh

--- scripts/01_prepare_reference.sh
+++ scripts/01_prepare_reference.sh
@@ -39,52 +39,7 @@ gatk CreateSequenceDictionary \
 echo "4a. Downloading dbSNP known sites..."
 
 wget -c https://ftp.ncbi.nlm.nih.gov/snp/latest_release/VCF/GCF_000001405.40.gz
-
-# =========================
-# 4b. Create contig rename map (NCBI → UCSC chr)
-# =========================
-echo "Creating contig rename map for dbSNP..."
-
-cat > ncbi_to_chr.txt << 'EOF'
-NC_000001.11	chr1
-NC_000002.12	chr2
-NC_000003.12	chr3
-NC_000004.12	chr4
-NC_000005.10	chr5
-NC_000006.12	chr6
-NC_000007.14	chr7
-NC_000008.11	chr8
-NC_000009.12	chr9
-NC_000010.11	chr10
-NC_000011.10	chr11
-NC_000012.12	chr12
-NC_000013.11	chr13
-NC_000014.9	chr14
-NC_000015.10	chr15
-NC_000016.10	chr16
-NC_000017.11	chr17
-NC_000018.10	chr18
-NC_000019.10	chr19
-NC_000020.11	chr20
-NC_000021.9	chr21
-NC_000022.11	chr22
-NC_000023.11	chrX
-NC_000024.10	chrY
-NC_012920.1	chrM
-EOF
-
-# =========================
-# 4c. Rename dbSNP contigs to match hg38 reference
-# =========================
-echo "Renaming dbSNP contigs to chr-style..."
-
-bcftools annotate \
-  --rename-chrs ncbi_to_chr.txt \
-  GCF_000001405.40.gz \
-  -Oz -o dbsnp_hg38_chr.vcf.gz
-
-tabix -p vcf dbsnp_hg38_chr.vcf.gz
-
+tabix -p vcf GCF_000001405.40.gz
 
 
 # =========================
