6ef811fa9c4981f3aad6eea81fcd453f05992fd6
Haneen Aljamal <hannoneh90@hotmail.com>
2026-03-06 00:34:52 +0300

    Update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+# project root
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
 # directories
-reads="/home/hannoneh90/variant-calling/reads"
-aligned_reads="/home/hannoneh90/variant-calling/aligned_reads"
-results="/home/hannoneh90/variant-calling/results"
-data="/home/hannoneh90/variant-calling/data"
-ref_dir="/home/hannoneh90/variant-calling/supporting_files/hg38"
+ref_dir="$PROJECT_DIR/supporting_files/hg38"
+reads="$PROJECT_DIR/reads"
+aligned_reads="$PROJECT_DIR/aligned_reads"
+results="$PROJECT_DIR/results"
+data="$PROJECT_DIR/data"
 
 # reference files
-ref="/home/hannoneh90/variant-calling/supporting_files/hg38/hg38.fa"
+ref="$ref_dir/hg38.fa"
 known_sites="$ref_dir/dbsnp_hg38.vcf.gz"
 
 # create directories
