7df5f2fd70dcd42118df0396ca79a9399b7e103b
Haneen Aljamal <hannoneh90@hotmail.com>
2026-03-06 00:30:40 +0300

    Update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# reference files
-ref="/home/hannoneh90/variant-calling/supporting_files/hg38/hg38.fa"
-known_sites="$ref_dir/dbsnp_hg38.vcf.gz"
-
 # directories
 reads="/home/hannoneh90/variant-calling/reads"
 aligned_reads="/home/hannoneh90/variant-calling/aligned_reads"
 results="/home/hannoneh90/variant-calling/results"
 data="/home/hannoneh90/variant-calling/data"
 ref_dir="/home/hannoneh90/variant-calling/supporting_files/hg38"
+
+# reference files
+ref="/home/hannoneh90/variant-calling/supporting_files/hg38/hg38.fa"
+known_sites="$ref_dir/dbsnp_hg38.vcf.gz"
 
 # create directories
 mkdir -p "$reads" "$aligned_reads" "$results" "$ref_dir" "$data"
