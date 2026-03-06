11d8b9411dadf5b1036805e2323e1ddd7016ca46
Haneen Aljamal <hannoneh90@hotmail.com>
2026-03-06 12:25:24 +0300

    Update 02_fastqc.sh

--- scripts/02_fastqc.sh
+++ scripts/02_fastqc.sh
@@ -2,6 +2,11 @@
 set -euo pipefail
 source "$(dirname "$0")/config.sh"
 
+if [ ! -f "$reads/SRR062634_1.fastq" ]; then
+    echo "Reads not found. Run 00_download_data.sh first."
+    exit 1
+fi
+
 echo "Step 2: Running FastQC"
 
 fastqc -t 4 ${reads}/SRR062634_1.fastq.gz ${reads}/SRR062634_2.fastq.gz -o ${reads}
