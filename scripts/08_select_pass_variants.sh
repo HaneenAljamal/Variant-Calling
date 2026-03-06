737fab6dfc9f3d38ae2055fdab6dcc8044789190
Haneen Aljamal <hannoneh90@hotmail.com>
2026-03-06 17:38:57 +0300

    Update 08_select_pass_variants.sh

--- scripts/08_select_pass_variants.sh
+++ scripts/08_select_pass_variants.sh
@@ -2,6 +2,11 @@
 set -euo pipefail
 source "$(dirname "$0")/config.sh"
 
+if [ ! -f "$results/SRR062634.filtered_variants.vcf.gz" ]; then
+    echo "Filtered VCF missing"
+    exit 1
+fi
+
 echo "step 8: PASS variants extractcion"
 
 bcftools view \
