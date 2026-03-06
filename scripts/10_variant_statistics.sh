dd7372b2b8b8e956535ba28cf52f9453a0b81b47
Haneen Aljamal <hannoneh90@hotmail.com>
2026-03-06 17:25:29 +0300

    Update 10_variant_statistics.sh

--- scripts/10_variant_statistics.sh
+++ scripts/10_variant_statistics.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+set -euo pipefail
+source "$(dirname "$0")/config.sh"
+
+echo "step 10: variant statistics"
+
 bcftools stats $results/SRR062634.pass_variants.vcf.gz > $results/variant_stats.txt
