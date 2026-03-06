7f93ee72cd9d7cc7b6275cb0b2cbd71dce028e50
Haneen Aljamal <hannoneh90@hotmail.com>
2026-03-06 17:24:52 +0300

    Update 09_annotation.sh

--- scripts/09_annotation.sh
+++ scripts/09_annotation.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+source "$(dirname "$0")/config.sh"
+
+echo "step 9: annotation"
+
 snpEff GRCh38.105 \
   $results/SRR062634.pass_variants.vcf.gz \
   > $results/SRR062634.annotated.vcf
